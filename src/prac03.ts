6a8040a763941b84c52b6b32281ea9696369718f
bot-man573 <itsu12king09@gmail.com>
2025-01-15 23:58:45 +0900

    タスクの期限をしっかり読み取れないからその部分を更新する。

--- src/prac03.ts
+++ src/prac03.ts
@@ -1,16 +1,16 @@
 import { Todo } from "./types";
-import { isOverdue } from "./utils/isOverdue";
+import { isOverdue, judgeOverdue } from "./utils/isOverdue";
 
 const todo1: Todo = {
   name: "TypeScriptの勉強",
   priority: 3,
   isDone: false,
-  deadline: new Date(2023, 9, 11, 9, 45),
+  deadline: new Date(2025, 1, 16, 1, 45),
 };
 
 const judge = (todo: Todo): void => {
-  const { deadline } = todo;
   console.log(isOverdue(todo)); // isOverdue関数を使用して結果を出力
+  console.log(judgeOverdue(todo)); // judgeOverdue関数を使用して結果を出力
 };
 
 judge(todo1);
--- src/utils/isOverdue.ts
+++ src/utils/isOverdue.ts
@@ -4,3 +4,19 @@ export const isOverdue = (todo: Todo): boolean => {
   const currentTime = new Date();
   return currentTime > todo.deadline && !todo.isDone;
 };
+
+export const judgeOverdue = (todo: Todo): string => {
+  const currentTime = new Date();
+  const remainTimeDifference = todo.deadline.getTime() - currentTime.getTime();
+  const hoursRemaining = Math.ceil(remainTimeDifference / (1000 * 60 * 60));
+  const overTimeDifference = currentTime.getTime() - todo.deadline.getTime();
+  const hoursOvering = Math.ceil(overTimeDifference / (1000 * 60 * 60));
+
+  if (currentTime > todo.deadline && !todo.isDone) {
+    return `【未】 ${todo.name}  （期限を ${hoursOvering} 時間超過）`;
+  } else if (todo.isDone) {
+    return `【済】${todo.name}` ;
+  } else {
+    return `【未】 ${todo.name} （期限まで残り ${hoursRemaining} 時間）`;
+  }
+};
