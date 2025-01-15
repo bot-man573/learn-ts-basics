07361925f03506e3dd59d687f82f2a65bed7b119
I.Otani <191228854+bot-man573@users.noreply.github.com>
2025-01-15 19:26:25 +0900

    dayjsを依存関係に追加し、日付処理のための新しい機能を実装

--- src/prac01.ts
+++ src/prac01.ts
@@ -1,3 +1,16 @@
-export {}; // ファイルをモジュールとして扱いスコープを分離するために必要
-const userName: string = "Bob";
-console.log(`Hey, ${userName}.`);
+export {};
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import "dayjs/locale/ja";
+
+dayjs.extend(weekday);
+dayjs.locale("ja");
+
+const dtFmt = "YYYY/MM/DD(ddd) HH:mm";
+const deadline: Date = new Date(2024, 10, 2, 11, 45);
+const createdAt: Date = new Date(); // 引数なしで現在日時を取得
+
+const str =
+  `期限 ${dayjs(deadline).format(dtFmt)}` +
+  `(登録日 ${dayjs(createdAt).format(dtFmt)})`;
+console.log(str);
---
+++ src/prac02.ts
@@ -0,0 +1,21 @@
+export {};
+import { Todo } from "./types"; // 型定義の読込み
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+
+const todo1: Todo = {
+  name: "TypeScriptの勉強",
+  priority: 3,
+  isDone: false,
+  deadline: new Date(2024, 9, 11, 9, 45),
+};
+
+const todo2: Todo = {
+  name: "基礎物理3の宿題",
+  priority: 1,
+  isDone: false,
+  deadline: new Date(2024, 9, 10, 16, 0),
+};
+
+console.log(JSON.stringify(todo1, null, 2));
+console.log(JSON.stringify(todo2, null, 2));
